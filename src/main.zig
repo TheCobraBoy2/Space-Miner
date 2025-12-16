1e2a114c60c075167b05e2ea5f85e3bd1b3266f3
TheCobraBoy2 <landoncs10@gmail.com>
2025-12-15 20:51:37 -0600

    Add helper methods for string formatting create gpa and assetPath buffer

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,6 @@
-// ------------------------------
-// ---        Imports         ---
-// ------------------------------
+// -------------------------------
+// ---         Imports         ---
+// -------------------------------
 const std = @import("std");
 const Space_Miner = @import("Space_Miner");
 const BoundedArray = @import("bounded_array").BoundedArray;
@@ -16,15 +16,74 @@ const Sounds = @import("sounds.zig");
 const Vector2 = rl.Vector2;
 const Math = std.math;
 
-// ------------------------------
-// ---       Constants        ---
-// ------------------------------
+// -------------------------------
+// ---        Constants        ---
+// -------------------------------
 const window_scale = 2;
 const window_size = Vector2.init(640 * window_scale, 480 * window_scale);
 const window_title = "Space Miner";
 const window_color = rl.Color.black;
+const target_fps = 60;
+const window_exit_key = rl.KeyboardKey.f9;
+const allocator: std.mem.Allocator = undefined;
 
+// ------------------------------
+// ---       Game State       ---
+// ------------------------------
+const GameState = struct {};
+var state: GameState = undefined;
+
+// -------------------------------
+// ---         Methods         ---
+// -------------------------------
+
+fn drawSum() void {}
+
+fn fmt(
+    buf: []u8,
+    comptime str: []const u8,
+    args: anytype,
+) ![]const u8 {
+    return std.fmt.bufPrint(buf, str, args);
+}
+
+fn aFmt(
+    alloc: std.mem.Allocator,
+    comptime str: []const u8,
+    args: anytype,
+) ![]const u8 {
+    return std.fmt.allocPrint(alloc, str, args);
+}
+
+// requires a path from workspace_root/assets
+fn assetPath(buf: []u8, path: []const u8) ![]const u8 {
+    return fmt(buf, "assets/{s}", .{path});
+}
+
+// ------------------------------
+// ---    Constant Methods    ---
+// ------------------------------
+fn update() void {}
+
+fn render() void {
+    var frame_arena = std.heap.ArenaAllocator.init();
+    defer frame_arena.deinit();
+    const A = frame_arena.allocator();
+    _ = A;
+
+    rl.drawText("Hello this is raylib in zig", 10, 10, 20, .white);
+}
+
+// ------------------------------
+// ---          Main          ---
+// ------------------------------
 pub fn main() !void {
+    // create our allocator
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    allocator = gpa.allocator();
+
     defer Textures.unloadAll();
     defer Sounds.unloadAll();
 
@@ -33,9 +92,24 @@ pub fn main() !void {
     defer rl.closeWindow();
     defer rl.closeAudioDevice();
 
+    rl.setExitKey(window_exit_key);
+    rl.setTargetFPS(target_fps);
+    var a_buf: [256]u8 = undefined; // buffer for assetPaths
+
+    const texture = try Textures.loadTexture(assetPath(&a_buf, "texturefile.png"));
+    _ = texture;
+
+    // Initialization of the game state
+    state = .{};
+
+    // Application loop
     while (!rl.windowShouldClose()) {
+        update();
+
         rl.beginDrawing();
         defer rl.endDrawing();
         rl.clearBackground(window_color);
+
+        render();
     }
 }
